e155cf686ef5fa2c4b2706525c35292474a73c58
Bruno Silva <bruno.s.iwamura@gmail.com>
2023-11-10 08:56:58 -0300

    Primeiro commit

---
+++ Aula11/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Aula11.Entities.Enums;
+
+namespace Aula11.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Aula11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Aula11.Entities;
+using Aula11.Entities.Enums;
+
+namespace Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { Id = 1080, Moment = DateTime.Now, Status =  OrderStatus.PendingPayment};
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
